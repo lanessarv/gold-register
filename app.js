b3a42165beace36e2407f6c0887e9b3784417916
lanessarv <lanessarv@gmail.com>
2026-04-26 15:20:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,37 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient(
   'https://qkpbmobfwwcygzjxehik.supabase.co',
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrcGJtb2Jmd3djeWd6anhlaGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjA4MzAsImV4cCI6MjA5Mjc5NjgzMH0.C0mPGmPiO24SvrsIAMs27qTR8X5VxfOuk9cbHQ2PIkk
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrcGJtb2Jmd3djeWd6anhlaGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjA4MzAsImV4cCI6MjA5Mjc5NjgzMH0.C0mPGmPiO24SvrsIAMs27qTR8X5VxfOuk9cbHQ2PIkk'
 )
 
-const loginBtn = document.getElementById('loginBtn')
-const signupBtn = document.getElementById('signupBtn')
+const loginScreen = document.getElementById('loginScreen')
+const app = document.getElementById('app')
 
-loginBtn.onclick = async () => {
+app.style.display = 'none'
+
+document.getElementById('signupBtn').onclick = async () => {
   const email = document.getElementById('loginEmail').value
   const password = document.getElementById('loginPassword').value
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password
-  })
+  const { error } = await supabase.auth.signUp({ email, password })
 
   if (error) {
     alert(error.message)
   } else {
-    location.reload()
+    alert('Account created. Now tap Login.')
   }
 }
 
-signupBtn.onclick = async () => {
+document.getElementById('loginBtn').onclick = async () => {
   const email = document.getElementById('loginEmail').value
   const password = document.getElementById('loginPassword').value
 
-  const { error } = await supabase.auth.signUp({
-    email,
-    password
-  })
+  const { error } = await supabase.auth.signInWithPassword({ email, password })
 
   if (error) {
     alert(error.message)
   } else {
-    alert("Account created. Now login.")
+    loginScreen.style.display = 'none'
+    app.style.display = 'block'
   }
 }
